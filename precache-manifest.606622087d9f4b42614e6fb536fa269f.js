d57b857dcbaf85fb3f58286d0a42f1153695c3e0
Saksham <sakshamsrini@gmail.com>
2022-05-11 03:51:27 +0530

    Updates

---
+++ precache-manifest.606622087d9f4b42614e6fb536fa269f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "031842fcc76375d40a137528898e66d8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8b1d2ba57f305cb1bc36",
+    "url": "./static/css/2.a6a636a0.chunk.css"
+  },
+  {
+    "revision": "27184acb85c2f176ae30",
+    "url": "./static/css/main.d850c1a7.chunk.css"
+  },
+  {
+    "revision": "8b1d2ba57f305cb1bc36",
+    "url": "./static/js/2.5af5d2b1.chunk.js"
+  },
+  {
+    "revision": "21b2dd38606ac28751db0d936591a095",
+    "url": "./static/js/2.5af5d2b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27184acb85c2f176ae30",
+    "url": "./static/js/main.f604dffb.chunk.js"
+  },
+  {
+    "revision": "e720711dd3e4e8b209d0",
+    "url": "./static/js/runtime-main.43c7aded.js"
+  },
+  {
+    "revision": "df8b84d5c24a3544c25001f3959d7a77",
+    "url": "./static/media/box.df8b84d5.png"
+  }
+]);
